8ab72b997af063ee450fc6a70ceef8f34edcbb2f
Fleench <social.glenn@flench.me>
2026-04-22 10:00:06 -0600

    Prep for CPP rewrite

--- ViewModels/MainWindowViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Spotify_Playlist_Manager.ViewModels;
-
-public partial class MainWindowViewModel : ViewModelBase
-{
-    public string Greeting { get; } = "Welcome to Avalonia! Hello World! I made another edit!";
-    
-}
--- ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace Spotify_Playlist_Manager.ViewModels;
-
-public class ViewModelBase : ObservableObject
-{
-}
